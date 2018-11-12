644f77f07ffc02ba420aeac7ac3b9b49689bed3c
Maxime Daniel <root@maxux.net>
2018-11-12 17:57:04 +0100

    Fix format using gofmt

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"errors"
 	"flag"
 	"fmt"
@@ -8,31 +9,30 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-    "bytes"
 
 	"crypto/ecdsa"
 	"github.com/dgrijalva/jwt-go"
 
+	"github.com/codahale/blake2"
+	"github.com/gomodule/redigo/redis"
 	"github.com/tidwall/resp"
-    "github.com/gomodule/redigo/redis"
-    "github.com/codahale/blake2"
 )
 
 var jwtPubKey *ecdsa.PublicKey
 
 type ZdbBackend struct {
-    Server redis.Conn
+	Server redis.Conn
 }
 
 type Session struct {
-    Authenticated bool
-    Username string
-    Backend *ZdbBackend
+	Authenticated bool
+	Username      string
+	Backend       *ZdbBackend
 }
 
 type Sessions struct {
-    List map[*resp.Conn]*Session
-    Backend *ZdbBackend
+	List    map[*resp.Conn]*Session
+	Backend *ZdbBackend
 }
 
 //
@@ -59,26 +59,26 @@ func cryptoInit() {
 // list of sessions
 //
 func NewSessions(backend *ZdbBackend) *Sessions {
-    return &Sessions{
-        List: make(map[*resp.Conn]*Session),
-        Backend: backend,
-    }
+	return &Sessions{
+		List:    make(map[*resp.Conn]*Session),
+		Backend: backend,
+	}
 }
 
 // single session
 func (ss *Sessions) NewSession() *Session {
-    return &Session{
-        Authenticated: false,
-        Username: "",
-        Backend: ss.Backend,
-    }
+	return &Session{
+		Authenticated: false,
+		Username:      "",
+		Backend:       ss.Backend,
+	}
 }
 
 // create a new Session based on a new connection
 func (ss *Sessions) SessionAccept(conn *resp.Conn) bool {
-    log.Printf("Creating a new session for [%v]\n", conn.RemoteAddr)
-    ss.List[conn] = ss.NewSession()
-    return true;
+	log.Printf("Creating a new session for [%v]\n", conn.RemoteAddr)
+	ss.List[conn] = ss.NewSession()
+	return true
 }
 
 // verify a jwt token is valid
@@ -94,10 +94,10 @@ func verifyToken(tokenStr string) (bool, string, error) {
 		return false, "", err
 	}
 
-    if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-        log.Printf("Token validated: %v", claims["username"])
-        return true, claims["username"].(string), nil
-    }
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		log.Printf("Token validated: %v", claims["username"])
+		return true, claims["username"].(string), nil
+	}
 
 	return false, "", nil
 }
@@ -106,30 +106,30 @@ func verifyToken(tokenStr string) (bool, string, error) {
 // AUTH
 //
 func (ss *Sessions) Authenticate(conn *resp.Conn, args []resp.Value) bool {
-    // retreive session from connection
-    session := ss.List[conn]
-    return session.Authenticate(conn, args)
+	// retreive session from connection
+	session := ss.List[conn]
+	return session.Authenticate(conn, args)
 }
 
 func (s *Session) Authenticate(conn *resp.Conn, args []resp.Value) bool {
 	if len(args) != 2 {
 		conn.WriteError(errors.New("Wrong number of arguments for 'AUTH' command"))
-        return true
-    }
+		return true
+	}
 
-    valid, username, err := verifyToken(args[1].String())
-    if !valid {
-        log.Printf("Authentication failed: %v\n", err)
+	valid, username, err := verifyToken(args[1].String())
+	if !valid {
+		log.Printf("Authentication failed: %v\n", err)
 		conn.WriteError(errors.New("Access denied"))
 
-        s.Authenticated = false
-        return true
-    }
+		s.Authenticated = false
+		return true
+	}
 
-    s.Username = username
-    s.Authenticated = true
+	s.Username = username
+	s.Authenticated = true
 
-    conn.WriteSimpleString("OK - " + s.Username)
+	conn.WriteSimpleString("OK - " + s.Username)
 	return true
 }
 
@@ -137,125 +137,122 @@ func (s *Session) Authenticate(conn *resp.Conn, args []resp.Value) bool {
 // INFO
 //
 func (ss *Sessions) Info(conn *resp.Conn, args []resp.Value) bool {
-    conn.WriteString("Gateway for 0-db (zdb) compatible clients\n")
+	conn.WriteString("Gateway for 0-db (zdb) compatible clients\n")
 	return true
 }
-
-
 
 //
 // EXISTS
 //
 func (ss *Sessions) Exists(conn *resp.Conn, args []resp.Value) bool {
-    // retreive session from connection
-    session := ss.List[conn]
-    return session.Exists(conn, args)
+	// retreive session from connection
+	session := ss.List[conn]
+	return session.Exists(conn, args)
 }
 
 func (s *Session) Exists(conn *resp.Conn, args []resp.Value) bool {
-    if !s.Authenticated {
-        log.Printf("Command EXISTS for unauthenticated user [%v]\n", conn.RemoteAddr)
-        conn.WriteError(errors.New("Permission denied"))
-        return true
-    }
+	if !s.Authenticated {
+		log.Printf("Command EXISTS for unauthenticated user [%v]\n", conn.RemoteAddr)
+		conn.WriteError(errors.New("Permission denied"))
+		return true
+	}
 
 	if len(args) != 2 {
 		conn.WriteError(errors.New("Wrong number of arguments for 'EXISTS' command"))
-        return true
-    }
+		return true
+	}
 
-    reply, err := s.Backend.Server.Do("EXISTS", args[1])
-    if err != nil {
-        log.Printf("Exists failed: %v\n", err)
-        conn.WriteError(errors.New("Cannot perform query right now"))
-        return true
-    }
+	reply, err := s.Backend.Server.Do("EXISTS", args[1])
+	if err != nil {
+		log.Printf("Exists failed: %v\n", err)
+		conn.WriteError(errors.New("Cannot perform query right now"))
+		return true
+	}
 
-    // forward value
-    conn.WriteInteger(int(reply.(int64)))
+	// forward value
+	conn.WriteInteger(int(reply.(int64)))
 
-    return true
+	return true
 }
 
 //
 // SET
 //
 func (ss *Sessions) Set(conn *resp.Conn, args []resp.Value) bool {
-    // retreive session from connection
-    session := ss.List[conn]
-    return session.Set(conn, args)
+	// retreive session from connection
+	session := ss.List[conn]
+	return session.Set(conn, args)
 }
 
 func (s *Session) DataValidator(key []byte, payload []byte) bool {
-    h := blake2.New(&blake2.Config{Size: 16})
-    h.Write(payload)
-    d := h.Sum(nil)
+	h := blake2.New(&blake2.Config{Size: 16})
+	h.Write(payload)
+	d := h.Sum(nil)
 
-    return (bytes.Compare(d, key) == 0)
+	return (bytes.Compare(d, key) == 0)
 }
 
 func (s *Session) Set(conn *resp.Conn, args []resp.Value) bool {
-    if !s.Authenticated {
-        log.Printf("Command SET for unauthenticated user [%v]\n", conn.RemoteAddr)
-        conn.WriteError(errors.New("Permission denied"))
-        return true
-    }
+	if !s.Authenticated {
+		log.Printf("Command SET for unauthenticated user [%v]\n", conn.RemoteAddr)
+		conn.WriteError(errors.New("Permission denied"))
+		return true
+	}
 
 	if len(args) != 3 {
 		conn.WriteError(errors.New("Wrong number of arguments for 'SET' command"))
-        return true
-    }
+		return true
+	}
 
-    if !s.DataValidator(args[1].Bytes(), args[2].Bytes()) {
-        log.Printf("Validator failed, payload hash doesn't match key\n")
-        conn.WriteError(errors.New("Unauthorized payload"))
-        return true
-    }
+	if !s.DataValidator(args[1].Bytes(), args[2].Bytes()) {
+		log.Printf("Validator failed, payload hash doesn't match key\n")
+		conn.WriteError(errors.New("Unauthorized payload"))
+		return true
+	}
 
-    reply, err := s.Backend.Server.Do("SET", args[1], args[2])
-    if err != nil {
-        log.Printf("SET failed: %v\n", err)
-        conn.WriteError(errors.New("Cannot perform query right now"))
-        return true
-    }
+	reply, err := s.Backend.Server.Do("SET", args[1], args[2])
+	if err != nil {
+		log.Printf("SET failed: %v\n", err)
+		conn.WriteError(errors.New("Cannot perform query right now"))
+		return true
+	}
 
-    // reply can be nul or key name
-    // checking type and forwarding according type
-    if reply == nil {
-        conn.WriteNull()
-        return true
-    }
+	// reply can be nul or key name
+	// checking type and forwarding according type
+	if reply == nil {
+		conn.WriteNull()
+		return true
+	}
 
-    value, ok := reply.([]uint8)
-    if ok {
-        conn.WriteBytes(value)
-        return true
-    }
+	value, ok := reply.([]uint8)
+	if ok {
+		conn.WriteBytes(value)
+		return true
+	}
 
-    // zdb replied something weird
-    log.Printf("Unexpected response: %v\n", reply)
-    conn.WriteError(errors.New("Unexpected response from server"))
+	// zdb replied something weird
+	log.Printf("Unexpected response: %v\n", reply)
+	conn.WriteError(errors.New("Unexpected response from server"))
 
-    return true
+	return true
 }
-
 
 //
 // backend
 //
 func NewBackend(server string) *ZdbBackend {
-    c, err := redis.Dial("tcp", server)
-    if err != nil {
-        log.Fatal(err)
-    }
+	c, err := redis.Dial("tcp", server)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    return &ZdbBackend{
-        Server: c,
-    }
+	return &ZdbBackend{
+		Server: c,
+	}
 }
 
 func (b *ZdbBackend) Close() {
-    b.Server.Close()
+	b.Server.Close()
 }
 
 var addrflag = flag.String("addr", ":16379", "listening address")
@@ -263,31 +260,31 @@ var backendflag = flag.String("backend", "127.0.0.1:9900", "zdb backend host/por
 
 func main() {
 	flag.Parse()
-    cryptoInit()
+	cryptoInit()
 
-    backend := NewBackend(*backendflag)
-    ss := NewSessions(backend)
+	backend := NewBackend(*backendflag)
+	ss := NewSessions(backend)
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, syscall.SIGINT)
 
 	defer func() {
 		log.Println("Closing connections")
-        backend.Close()
+		backend.Close()
 		os.Exit(0)
 	}()
 
 	r := resp.NewServer()
-    r.AcceptFunc(ss.SessionAccept)
-    r.HandleFunc("auth", ss.Authenticate)
-    r.HandleFunc("exists", ss.Exists)
-    r.HandleFunc("set", ss.Set)
-    r.HandleFunc("info", ss.Info)
+	r.AcceptFunc(ss.SessionAccept)
+	r.HandleFunc("auth", ss.Authenticate)
+	r.HandleFunc("exists", ss.Exists)
+	r.HandleFunc("set", ss.Set)
+	r.HandleFunc("info", ss.Info)
 
 	go func() {
 		<-c
 		log.Println("closing kvs")
-        backend.Close()
+		backend.Close()
 		os.Exit(0)
 	}()
 
